bc4e13bbcda8ed395a3fbd7f45167290827bc349
AntonioHugo21 <antoniohugo.pierre@gmail.com>
2024-09-09 10:13:17 -0300

    conversao de reais para dolares

---
+++ atividade08.py
@@ -0,0 +1,8 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+
+valor_reais = float(input("digite o valor em reais"))
+
+
+conversao = valor_reais/5.60
+
+print(f"o valor  final é ={conversao}")
